b862cd35f3680cf8d7c441a65d38cd3f54c191b1
Juan Hernandez <juanjhernandezs1@gmail.com>
2025-09-04 18:04:33 -0500

    Feat: Edit Project, add header (Part2)

--- Taller4-Angular/src/app/app.component.ts
+++ Taller4-Angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BuscadorComponent } from './buscador/buscador.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BuscadorComponent],
+  imports: [RouterOutlet, BuscadorComponent, HeaderComponent, LogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
