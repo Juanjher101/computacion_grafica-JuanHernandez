022459db46f30c38a183def05ccf4e435c147389
Juan Hernandez <juanjhernandezs1@gmail.com>
2025-09-04 17:33:35 -0500

    #

--- Taller4-Angular/src/app/app.component.ts
+++ Taller4-Angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BuscadorComponent } from './buscador/buscador.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BuscadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
